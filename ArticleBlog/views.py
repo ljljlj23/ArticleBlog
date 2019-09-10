f759d8c28a066d431e6f2ed5763687b4f0fc6747
lj <1074819863@qq.com>
2019-09-10 20:44:09 +0800

    博客项目01 模板继承

--- ArticleBlog/views.py
+++ ArticleBlog/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def test(request):
+def hello(request):
     return HttpResponse('hello')
+
+def about(request):
+    return render(request,'about.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def listpic(request):
+    return render(request,'listpic.html')
+
+def newslistpic(request):
+    return render(request,'newslistpic.html')
+
+def base(request):
+    return render(request,'base.html')
